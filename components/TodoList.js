b2535aae9dfcb5b20175c7abae4a12d8f40178e4
patirosa95 <patirosa995@gmail.com>
2021-01-20 11:53:22 -0300

    created component todolist.js

---
+++ components/TodoList.js
@@ -0,0 +1,17 @@
+import React, { Component, Components } from 'react';
+import { FlatList, View } from 'react-native';
+
+class TodoList extends Component{
+    render(){
+        return(
+            <View>
+                <FlatList
+                
+                
+                />
+            </View>
+        );
+    }
+}
+
+export default TodoList;
